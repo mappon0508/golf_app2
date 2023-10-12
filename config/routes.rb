ce5818820caf4f0be718a33a3e6b44c52b3bba77
mappon0508 <shomabirdie@icloud.com>
2023-10-12 16:30:26 +0900

    スコア登録機能とスコア編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,23 @@ Rails.application.routes.draw do
   delete "/logout",                                   to: "sessions#destroy"
 
 
-  get "/hole/:id",                                    to: "holes#new",    as: "hole_new"
+  get "/hole/:id",                                    to: "holes#new",             as: "hole_new"
   post "/holes",                                      to: "holes#create"
-  get "/hole/:golf_course_id/edit",                   to: "holes#edit",     as: "hole_edit"
-  patch "holes/:golf_course_id",                      to: "holes#update",   as: "hole_update"
+  get "/hole/:golf_course_id/edit",                   to: "holes#edit",            as: "hole_edit"
+  patch "holes/:golf_course_id",                      to: "holes#update",          as: "hole_update"
 
+  get "/golf_play_records/:id/new",                   to: "golf_play_records#new", as: "new_golf_play_record"
+
+  get "/score/:id/new",                               to: "scores#new",            as: "new_score"
+  get "/score/:id/:hole_id/new",                      to: "scores#next_new",       as: "new_scores"
+  post "/score/create",                               to: "scores#create",         as: "create_score"
+  get "/scores/:id/",                                 to: "scores#index",          as: "scores"
   
+  get "/past_scores",                                 to: "past_scores#index"
+  get "/past_score/:id",                              to: "past_scores#show",      as: "past_score"
+
+  resources :scores,  except: [:new,:create, :index]
+  resources :golf_play_records,  except: [:new]
   resources :golf_courses
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_082239) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_130841) do
   create_table "golf_courses", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_golf_courses_on_user_id"
+  end
+
+  create_table "golf_play_records", force: :cascade do |t|
+    t.integer "golf_course_id", null: false
+    t.date "play_day", null: false
+    t.integer "weather", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["golf_course_id"], name: "index_golf_play_records_on_golf_course_id"
   end
 
   create_table "holes", force: :cascade do |t|
@@ -26,6 +35,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_082239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["golf_course_id"], name: "index_holes_on_golf_course_id"
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "golf_play_record_id", null: false
+    t.integer "content"
+    t.integer "tee_shot", default: 0, null: false
+    t.integer "second_shot_distance", default: 0, null: false
+    t.integer "approach_shot", default: 0, null: false
+    t.integer "approach", default: 0, null: false
+    t.integer "bunker_save", default: 0, null: false
+    t.integer "long_putt", default: 0, null: false
+    t.integer "short_putt", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "hole_id", null: false
+    t.integer "putt"
+    t.index ["golf_play_record_id"], name: "index_scores_on_golf_play_record_id"
+    t.index ["hole_id"], name: "index_scores_on_hole_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_082239) do
   end
 
   add_foreign_key "golf_courses", "users"
+  add_foreign_key "golf_play_records", "golf_courses"
   add_foreign_key "holes", "golf_courses"
+  add_foreign_key "scores", "golf_play_records"
+  add_foreign_key "scores", "holes"
 end
